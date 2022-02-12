6f6ce5b1947f9e0e2fc1f023cd440a6c72107fbe
Vinicius Tavares Assis <vtassis@tjes.jus.br>
2022-02-12 18:38:07 -0300

    Atualizacao Readme e retirada de comentarios

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # calculadora-vue-spring-boot
+
+## Backend
+Desenvolvido utilizando Spring Boot é uma calculadora feita com o padrão SOAP.
+Configurado para rodar na porta 8080, deve ser iniciado antes do frontend
+
+## Frontend
+Desenvolvido utilizando a biblioteca Vue.js
+Para rodar `npm run serve`
--- frontend/src/services/CalculadoraService.js
+++ frontend/src/services/CalculadoraService.js
@@ -33,20 +33,9 @@ class CalculadoraService {
                             </gs:${operacao}>
                         </soapenv:Body>
                         </soapenv:Envelope>`;
-            /* let xmls = '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"'+
-                                    'xmlns:gs="http://example.com/calculadora">'+
-                        '<soapenv:Header/>'+
-                        '<soapenv:Body>'+
-                            '<'+operacao+'>'+
-                                '<numero01>'+data.num1+'</numero01>'+
-                                '<numero02>'+data.num2+'</numero02>'+
-                            '</'+operacao+'>'+
-                        '</soapenv:Body>'+
-                        '</soapenv:Envelope>'; */
             const options = {
                 method: "POST",
                 url: "http://localhost:8080/ws/",
-                /* body: xmls, */
                 data: xmls,
                 headers: {
                     'Content-Type': 'text/xml'
@@ -68,8 +57,6 @@ class CalculadoraService {
             var texto = dados.split("ns2:resultado\":[\"");
             var resultado = texto[1].split("\"");
             return resultado[0];
-            /* url: "http://localhost:8080/ws/calculadora.wsdl", */
-            /* "Access-Control-Allow-Origin": "*" */
             
         } catch (error) {
             console.error(error)
